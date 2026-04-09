8ddac2f70fdbcac1df5f378c510ece7d93c6466e
Stiven Cfarku <s.cfarku@studenti.poliba.it>
2026-04-09 16:13:44 +0200

    Add files via upload

---
+++ authController.js
@@ -0,0 +1,67 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+exports.register = async (req, res) => { 
+  try {
+    const { username, password } = req.body; 
+    
+    const existingUser = await User.findOne({ username }); 
+    if (existingUser) return res.status(400).json({ message: "Username già in uso" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+    res.json({ success: true, message: "Registrazione completata!" });
+  } catch (err) {
+    res.status(500).json({ message: "Errore durante la registrazione" }); 
+  } 
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }); 
+    if (!user) return res.status(400).json({ message: "Utente non trovato" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Password errata" });
+
+    res.cookie('sessione_utente', user.username, { 
+      httpOnly: true,
+      secure: true,  
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24
+    });
+
+    res.json({ success: true, message: "Login effettuato!", username: user.username });
+  }  catch (err) {
+  // Questo stamperà l'errore specifico (es. Duplicate Key o Connection Timeout) 
+  // nei log di Render
+  console.error("ERRORE DURANTE LA REGISTRAZIONE:", err); 
+  
+  res.status(500).json({ 
+    message: "Errore durante la registrazione",
+    error: err.message // Aggiungi questo per vedere l'errore nel browser
+  });
+
+};
+
+exports.checkSession = (req, res) => {
+  const utenteLoggato = req.cookies.sessione_utente;
+  if (utenteLoggato) {
+    res.json({ loggedIn: true, username: utenteLoggato });
+  } else {
+    res.json({ loggedIn: false });
+  }
+};
+
+exports.logout = (req, res) => {
+  res.clearCookie('sessione_utente', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none'
+  });
+  res.json({ success: true, message: "Logout effettuato" });
+};
