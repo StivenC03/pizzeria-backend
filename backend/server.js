8d86b8383f6c6ab92c4e22a4a85bfe8910b269e1
Stiven Cfarku <s.cfarku@studenti.poliba.it>
2026-05-19 10:25:51 +0200

    aggiornamento env server

--- backend/server.js
+++ backend/server.js
@@ -3,37 +3,46 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-// Import rotte
+
 const authRoutes = require('./routes/authRoutes');
 const menuRoutes = require('./routes/menuRoutes');
 const prenotazioneRoutes = require('./routes/prenotazioneRoutes');
 
-// Import controller per inizializzazione
+
 const menuController = require('./controllers/menuController');
 
-const app = express();
-const port = process.env.PORT || 3001;
+
+const app = express(); 
+const PORT = process.env.PORT || 5000;
+const dbURI = process.env.MONGO_URI;
+
 
 app.use(cors({
   origin: function(origin, callback){ return callback(null, true); },
   credentials: true 
 }));
-app.use(express.json());
-app.use(cookieParser());
 
-// Connessione DB
-mongoose.connect('mongodb+srv://stiven:stiven2003@cluster0.vz9pbpw.mongodb.net/?appName=Cluster0')
+
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    
     console.log("Connesso a MongoDB");
-    menuController.inizializzaMenu(); // Popolamento automatico
+    
+    menuController.inizializzaMenu(); 
   })
   .catch(err => console.error("Errore di connessione", err));
 
-// Utilizzo Rotte
-app.use('/api', authRoutes);
-app.use('/api', menuRoutes);
-app.use('/api', prenotazioneRoutes);
 
-app.listen(port, () => {
-  console.log(`Server modulare in ascolto sulla porta ${port}`);
+app.use('/api', authRoutes); 
+app.use('/api', menuRoutes); 
+app.use('/api', prenotazioneRoutes); 
+
+
+app.listen(PORT, () => {
+
+  console.log(`Server modulare in ascolto sulla porta ${PORT}`);
 });
