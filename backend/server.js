84150a8a8789d084b73190f7de199635968be67d
Stiven Cfarku <s.cfarku@studenti.poliba.it>
2026-05-19 11:54:18 +0200

    akfnoaelknsm

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const menuController = require('./controllers/menuController');
 const app = express(); 
 const PORT = process.env.PORT || 5000;
 const dbURI = process.env.MONGO_URI;
-
+// Aggiungi questo in server.js
+app.use((req, res, next) => {
+    console.log(`[DEBUG] Ricevuta richiesta: ${req.method} ${req.url}`);
+    next();
+});
 
 
 app.use(cors({
@@ -38,6 +42,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     menuController.inizializzaMenu(); 
   })
   .catch(err => console.error("Errore di connessione", err));
+
+
 
 
 app.use('/api', authRoutes); 
