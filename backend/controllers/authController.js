1c562d5d5eb70876c93197432e0b72f1a7df2e7c
Stiven Cfarku <s.cfarku@studenti.poliba.it>
2026-05-19 11:11:17 +0200

    correzione 2

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,7 +37,7 @@ exports.login = async (req, res) => {
 
    res.cookie('token', token, { 
       httpOnly: true,                 
-      secure: process.env.NODE_ENV === 'production', 
+      secure: true, 
       sameSite: 'none',             
       maxAge: 3600000                 
     });
