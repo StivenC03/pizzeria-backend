2eb96c1846288bec58ed76d2c1822853184e0d5b
Stiven Cfarku <s.cfarku@studenti.poliba.it>
2026-05-19 11:37:58 +0200

    removed protection routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,7 +9,6 @@ router.post('/login', authController.login);
 router.get('/check-session', authController.checkSession);
 
 
-router.use(authMiddleware);
 router.post('/logout', authController.logout);
 
 module.exports = router;
