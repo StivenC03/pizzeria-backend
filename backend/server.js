4087bc0817f98558b61cf9e2130fdded7600eaca
Stiven Cfarku <s.cfarku@studenti.poliba.it>
2026-05-26 10:36:16 +0200

    Correzione server post socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const http = require('http');
 const { Server } = require('socket.io');
 
@@ -10,18 +12,20 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://pizzeria-frontend-xbfp.onrender.com",
-    methods: ["GET", "POST", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
   }
 });
 
 app.set('io', io);
 
+
 app.use(cors({
     origin: "https://pizzeria-frontend-xbfp.onrender.com", 
     credentials: true 
 }));
 app.use(express.json());
+app.use(cookieParser()); 
 
 const authRoutes = require('./routes/authRoutes');
 const menuRoutes = require('./routes/menuRoutes');
@@ -31,9 +35,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/menu', menuRoutes);
 app.use('/api/prenotazioni', prenotazioneRoutes);
 
-mongoose.connect(process.env.MONGO_URI || 'tua_stringa_di_connessione')
-  .then(() => console.log('MongoDB Connesso'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connesso con successo'))
+  .catch(err => console.error('Errore di connessione a MongoDB:', err));
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server avviato sulla porta ${PORT}`));
